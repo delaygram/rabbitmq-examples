71d4b8ffe52a99d722ce846239c07ffa63f2437d
Rick Meels <rick.meels@gmail.com>
2022-05-25 15:54:23 +0200

    added basic receiver and sender

---
+++ src/main/java/com/delaygram/amqp/eventbusexample/EventBusExampleApplication.java
@@ -0,0 +1,33 @@
+package com.delaygram.amqp.eventbusexample;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class EventBusExampleApplication {
+
+    @Profile("usage_message")
+    @Bean
+    public CommandLineRunner usage() {
+        return args -> {
+            System.out.println("This app uses Spring Profiles to control its behavior.\n");
+            System.out.println("Sample usage: java -jar event-bus-example-2.7.0.jar --spring.profiles.active=hello-world,sender");
+        };
+    }
+
+    @Profile("!usage_message")
+    @Bean
+    public CommandLineRunner tutorial() {
+        return new EventBusExampleRunner();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventBusExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/delaygram/amqp/eventbusexample/EventBusExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delaygram.amqp.eventbusexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBusExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
