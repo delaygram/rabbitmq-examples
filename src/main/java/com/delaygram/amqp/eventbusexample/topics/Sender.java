0f876d12ee0aeeabe95d6f481c41d09540fa5a7d
Rick Meels <rick.meels@gmail.com>
2022-05-27 16:09:36 +0200

    added topics with specified routing keys concept

---
+++ src/main/java/com/delaygram/amqp/eventbusexample/topics/Sender.java
@@ -0,0 +1,37 @@
+package com.delaygram.amqp.eventbusexample.topics;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Sender {
+
+    @Autowired
+    private RabbitTemplate template;
+
+    @Autowired
+    private TopicExchange topic;
+
+    AtomicInteger index = new AtomicInteger(0);
+    AtomicInteger count = new AtomicInteger(0);
+
+    private final String[] keys = {"quick.orange.rabbit", "lazy.orange.elephant", "quick.orange.fox",
+            "lazy.brown.fox", "lazy.pink.rabbit", "quick.brown.fox"};
+
+    @Scheduled(fixedDelay = 1000, initialDelay = 500)
+    public void send() {
+        StringBuilder builder = new StringBuilder("Hello to ");
+        if(index.incrementAndGet() == keys.length) {
+            index.set(0);
+        }
+        String key = keys[index.get()];
+        builder.append(key).append(' ');
+        builder.append(count.incrementAndGet());
+        String message = builder.toString();
+        template.convertAndSend(topic.getName(), key, message);
+        System.out.println(" [x] Sent '" + message + "'");
+    }
+}
